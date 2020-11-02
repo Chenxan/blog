ddf03abd230fe7efd4bc733ba90adab424c8d058
Chenxan <2277587992@qq.com>
2020-11-02 20:33:12 +0800

    '数据库配置'

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'port': '3306',
+        'user': 'chen',
+        'PASSWORD': '123456',
+        'NAME': 'blog'
     }
 }
 
